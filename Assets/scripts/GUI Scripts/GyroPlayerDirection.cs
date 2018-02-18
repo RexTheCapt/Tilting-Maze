3680685de919b32bf9ca379ba35035f3ff8c81fa
RexTheCapt <RexTheCapt@gmail.com>
2018-02-18 01:29:56 +0100

    Added a velocity direction to the gyro disk

---
+++ Assets/scripts/GUI Scripts/GyroPlayerDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroPlayerDirection : MonoBehaviour
+{
+	private GameObject playerGameObject;
+	public GameObject dot;
+	public float multiplier = 1f;
+	public Vector2 offset = new Vector2();
+
+	void Start()
+	{
+		playerGameObject = GameObject.Find("Player");
+		if(playerGameObject != null)
+		{
+			Debug.Log("Player found");
+		}
+		else
+		{
+			Debug.Log("Player not found");
+			gameObject.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		Rigidbody rb = playerGameObject.GetComponent<Rigidbody>();
+		Vector2 v = new Vector2((rb.velocity.x * multiplier) + offset.x, (rb.velocity.z * multiplier) + offset.y);
+		transform.localPosition = v;
+	}
+}
